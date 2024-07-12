2b2848289540fa521b6354c1824df7f5f420d996
David Sánchez <david.sanchez@civica-soft.com>
2024-07-12 10:41:40 +0000

    Se crea dim_customers

--- models/staging/sales/stg_sales_sch_customers.sql
+++ models/staging/sales/stg_sales_sch_customers.sql
@@ -4,29 +4,18 @@
   )
 }}
 
-WITH src_sales_customers AS (
-    SELECT * 
-    FROM {{ source('sales', 'customers') }}
-    ),
 
-/*
-renamed_casted AS (
-    SELECT
-          CUSTOMER_ID
-        , FIRST_NAME
-        , LAST_NAME
-        , PHONE
-        , EMAIL
-        , STREET
-        , CITY
-        , STATE
-        , ZIP_CODE
-        , FIVETRAN_DELETED
-        , FIVETRAN_SYNCED
-    FROM src_sales_customers
-    )
-*/
-renamed_casted AS (SELECT * FROM src_sales_customers)
-SELECT * FROM renamed_casted
-
+SELECT
+      CUSTOMER_ID
+    , FIRST_NAME
+    , LAST_NAME
+    , PHONE
+    , EMAIL
+    , STREET
+    , cast(TRIM(REGEXP_SUBSTR(STREET, '^[0-9]+')) as integer) as STREET_NUMBER
+    , TRIM(SUBSTRING(STREET, LEN(STREET_NUMBER)+1)) as STREET_NAME
+    , CITY
+    , STATE
+    , ZIP_CODE
+FROM {{ source('sales', 'customers') }}
 
